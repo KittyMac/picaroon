a27e013bca3b2b77bc40532127eb9b91da64f430
Rocco Bowling <rocco@chimerasw.com>
2021-03-13 23:54:38 -0500

    setting swift tools version to 5.1.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1.0
 
 import PackageDescription
 
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
 		.package(url: "https://github.com/KittyMac/Flynn.git", .branch("master")),
-		.package(name: "Socket", url: "https://github.com/IBM-Swift/BlueSocket.git", .upToNextMinor(from: "1.0.0")),
+		.package(url: "https://github.com/IBM-Swift/BlueSocket.git", .upToNextMinor(from: "1.0.0")),
 		.package(url: "https://github.com/KittyMac/Ipecac.git", .branch("master")),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
     ],
