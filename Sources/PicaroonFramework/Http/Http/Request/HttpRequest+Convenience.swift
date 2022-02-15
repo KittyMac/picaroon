2419e07a05b4a8d1b5ea8ebc188e5bd47e4a72b5
Rocco Bowling <rocco@chimerasw.com>
2022-02-15 13:38:29 -0500

    disable gzip support if debug builds

--- Sources/PicaroonFramework/Http/Http/Request/HttpRequest+Convenience.swift
+++ Sources/PicaroonFramework/Http/Http/Request/HttpRequest+Convenience.swift
@@ -5,6 +5,10 @@ import Spanker
 
 public extension HttpRequest {
     var supportsGzip: Bool {
+        #if DEBUG
+        return false
+        #else
         return acceptEncoding?.contains("gzip") == true
+        #endif
     }
 }
