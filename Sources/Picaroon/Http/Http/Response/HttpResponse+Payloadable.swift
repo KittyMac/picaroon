f84cb396fdce65bc8f4a547b1b52b086a7cf233f
Rocco Bowling <rocco@chimerasw.com>
2022-10-17 09:23:54 -0400

    updated for latest hitch

--- Sources/Picaroon/Http/Http/Response/HttpResponse+Payloadable.swift
+++ Sources/Picaroon/Http/Http/Response/HttpResponse+Payloadable.swift
@@ -3,7 +3,7 @@ import Foundation
 import Hitch
 
 public protocol Payloadable {
-    func using(_ block: (UnsafePointer<UInt8>?, Int) -> ())
+    func using<T>(_ block: (UnsafePointer<UInt8>?, Int) -> T?) -> T?
 }
 
 extension HalfHitch: Payloadable {
@@ -15,33 +15,33 @@ extension Hitch: Payloadable {
 }
 
 extension Data: Payloadable {
-    public func using(_ block: (UnsafePointer<UInt8>?, Int) -> ()) {
-        withUnsafeBytes { unsafeRawBufferPointer in
+    public func using<T>(_ block: (UnsafePointer<UInt8>?, Int) -> T?) -> T? {
+        return withUnsafeBytes { unsafeRawBufferPointer in
             let unsafeBufferPointer = unsafeRawBufferPointer.bindMemory(to: UInt8.self)
-            guard let bytes = unsafeBufferPointer.baseAddress else { return }
-            block(bytes, count)
+            guard let bytes = unsafeBufferPointer.baseAddress else { return nil }
+            return block(bytes, count)
         }
     }
 }
 
 extension StaticString: Payloadable {
-    public func using(_ block: (UnsafePointer<UInt8>?, Int) -> ()) {
-        block(utf8Start, utf8CodeUnitCount)
+    public func using<T>(_ block: (UnsafePointer<UInt8>?, Int) -> T?) -> T? {
+        return block(utf8Start, utf8CodeUnitCount)
     }
 }
 
 extension String: Payloadable {
-    public func using(_ block: (UnsafePointer<UInt8>?, Int) -> ()) {
-        withCString { bytes in
+    public func using<T>(_ block: (UnsafePointer<UInt8>?, Int) -> T?) -> T? {
+        return withCString { bytes in
             var ptr = bytes
             while ptr.pointee != 0 {
                 ptr += 1
             }
             let count = ptr - bytes
             
-            bytes.withMemoryRebound(to: UInt8.self, capacity: count) { ptr in
-                block(ptr, count)
-            }            
+            return bytes.withMemoryRebound(to: UInt8.self, capacity: count) { ptr in
+                return block(ptr, count)
+            }
         }
     }
 }
