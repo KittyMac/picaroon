8a3cc62d9776253da662f66dd237c8c500606506
Rocco Bowling <rocco@chimerasw.com>
2025-10-10 12:03:32 -0400

    fix android compile

--- Makefile
+++ Makefile
@@ -32,7 +32,7 @@ profile: clean
 benchmark:
 	/opt/homebrew/bin/wrk -t 4 -c 100 http://localhost:8080/hello/world
 
-release:
+linux:
 	-DOCKER_HOST=ssh://rjbowli@192.168.111.203 docker buildx create --name cluster_builder203 --platform linux/amd64
 	-docker buildx create --name cluster_builder203 --platform linux/arm64 --append
 	-docker buildx use cluster_builder203
@@ -42,3 +42,7 @@ release:
 	swift package resolve
 	docker buildx build --platform linux/arm64 --push -t kittymac/picaroon .
 	#linux/amd64,
+
+android:
+	ANDROID_NDK_HOME=~/Downloads/android-ndk-r27d ~/Library/org.swift.swiftpm/swift-sdks/swift-6.2-RELEASE-android-0.1.artifactbundle/swift-android/scripts/setup-android-sdk.sh
+	swiftly run swift build  --configuration=release --swift-sdk aarch64-unknown-linux-android28 +6.2
--- Sources/Picaroon/DNS/DNS.swift
+++ Sources/Picaroon/DNS/DNS.swift
@@ -45,27 +45,41 @@ public class DNS: Actor {
             while ptr != nil {
                 defer { ptr = ptr?.pointee.ifa_next }
 
-                guard let interface = ptr?.pointee else { return "" }
-                let addrFamily = interface.ifa_addr.pointee.sa_family
+                guard let interface = ptr?.pointee else { return nil }
+                guard let ifa_addr = interface.ifa_addr else { return nil }
+                
+                let addrFamily = ifa_addr.pointee.sa_family
                 if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
 
                     // wifi = ["en0"]
                     // wired = ["en2", "en3", "en4"]
                     // cellular = ["pdp_ip0","pdp_ip1","pdp_ip2","pdp_ip3"]
 
-                    let name: String = String(cString: (interface.ifa_name))
-                    if  name == "en0" || name == "en2" || name == "en3" || name == "en4" || name == "pdp_ip0" || name == "pdp_ip1" || name == "pdp_ip2" || name == "pdp_ip3" {
-                        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-                        
-                        getnameinfo(interface.ifa_addr,
-                                    socklen_t(MemoryLayout<sockaddr>.size),
-                                    &hostname,
-                                    socklen_t(hostname.count),
-                                    nil,
-                                    0,
-                                    NI_NUMERICHOST)
-
-                        address = String(cString: hostname)
+                    if let ifa_name = interface.ifa_name {
+                        let name: String = String(cString: (ifa_name))
+                        if  name == "en0" || name == "en2" || name == "en3" || name == "en4" || name == "pdp_ip0" || name == "pdp_ip1" || name == "pdp_ip2" || name == "pdp_ip3" {
+                            var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                            
+#if os(Android)
+                            getnameinfo(ifa_addr,
+                                        socklen_t(MemoryLayout<sockaddr>.size),
+                                        &hostname,
+                                        Int(socklen_t(hostname.count)),
+                                        nil,
+                                        0,
+                                        NI_NUMERICHOST)
+#else
+                            getnameinfo(ifa_addr,
+                                        socklen_t(MemoryLayout<sockaddr>.size),
+                                        &hostname,
+                                        socklen_t(hostname.count),
+                                        nil,
+                                        0,
+                                        NI_NUMERICHOST)
+#endif
+                            
+                            address = String(cString: hostname)
+                        }
                     }
                 }
             }
