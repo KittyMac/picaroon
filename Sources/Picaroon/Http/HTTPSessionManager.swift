f7da987edb72af05fa2271ac4dc64a4086d92d85
Rocco Bowling <rocco@chimerasw.com>
2026-06-24 11:04:54 -0400

    revert change to httpMaximumConnectionsPerHost

--- Sources/Picaroon/Http/HTTPSessionManager.swift
+++ Sources/Picaroon/Http/HTTPSessionManager.swift
@@ -35,7 +35,11 @@ public class HTTPSessionManager: Actor {
         for _ in 0..<maxConcurrentSessions {
             let config = URLSessionConfiguration.ephemeral
             config.timeoutIntervalForRequest = 20.0
+#if os(Android)
             config.httpMaximumConnectionsPerHost = 1
+#else
+            config.httpMaximumConnectionsPerHost = min(max(Flynn.cores * 3, 4), 32)
+#endif
             config.urlCache = nil
             config.requestCachePolicy = .reloadIgnoringLocalAndRemoteCacheData
             config.httpCookieAcceptPolicy = .always
