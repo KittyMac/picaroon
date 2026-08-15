230e0c5ac808dd131fff10ed977043741e6aed09
Rocco Bowling <rocco@chimerasw.com>
2026-08-15 15:13:18 -0400

    remove session flush

--- Sources/Picaroon/Http/HTTPTaskManager.swift
+++ Sources/Picaroon/Http/HTTPTaskManager.swift
@@ -182,17 +182,15 @@ internal class HTTPTaskManager: Actor {
                     #endif
                     
                     let localNewRequest = newRequest
-                    session.flush {
-                        Flynn.Timer(timeInterval: 1.0, immediate: false, repeats: false, self) { [weak self] timer in
-                            guard let self = self else { return returnCallback(nil, nil, nil) }
-                            self.beResume(session: session,
-                                          request: localNewRequest,
-                                          proxy: proxy,
-                                          timeoutRetry: timeoutRetry - 1,
-                                          retryAnyError: retryAnyError,
-                                          self,
-                                          returnCallback)
-                        }
+                    Flynn.Timer(timeInterval: 1.0, immediate: false, repeats: false, self) { [weak self] timer in
+                        guard let self = self else { return returnCallback(nil, nil, nil) }
+                        self.beResume(session: session,
+                                      request: localNewRequest,
+                                      proxy: proxy,
+                                      timeoutRetry: timeoutRetry - 1,
+                                      retryAnyError: retryAnyError,
+                                      self,
+                                      returnCallback)
                     }
                     return
                 }
